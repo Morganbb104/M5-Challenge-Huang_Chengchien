8b1a8d5c5ee48ea9ab298a39d72a6b6720b9447f
scouring pad <hcc780921@gmail.com>
2022-08-02 06:59:47 -0700

    deleting starter code

--- starter/gamestore/src/main/java/com/trilogyed/gamestoreRegistry/repository/ProcessingFeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trilogyed.gamestoreRegistry.repository;
-
-import com.trilogyed.gamestoreRegistry.model.ProcessingFee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProcessingFeeRepository extends JpaRepository<ProcessingFee, String> {
-}
--- starter/gamestore/src/main/java/com/trilogyed/gamestoreRegistry/repository/TaxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trilogyed.gamestoreRegistry.repository;
-
-import com.trilogyed.gamestoreRegistry.model.Tax;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaxRepository extends JpaRepository<Tax, String> {
-}
