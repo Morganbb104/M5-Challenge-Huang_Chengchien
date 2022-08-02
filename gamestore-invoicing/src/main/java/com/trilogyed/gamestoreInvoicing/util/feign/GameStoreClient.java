c2769200142c8712b50d4bc68915c3603be1fc39
scouring pad <hcc780921@gmail.com>
2022-08-02 14:53:34 -0700

    fixed project structur

--- gamestore-invoicing/src/main/java/com/trilogyed/gamestoreInvoicing/util/feign/GameStoreClient.java
+++ gamestore-invoicing/src/main/java/com/trilogyed/gamestoreInvoicing/util/feign/GameStoreClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(name="game-tore-catalog")
+@FeignClient(name="game-store-catalog")
 public interface GameStoreClient {
 
     //console
